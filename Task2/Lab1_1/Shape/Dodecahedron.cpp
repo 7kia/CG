20d63c44a676a33637376f680d1715813d00c318
7kia <il7kia@yandex.ru>
2016-08-02 18:16:04 +0400

    Draw half Dodecahedron

--- Task2/Lab1_1/Shape/Dodecahedron.cpp
+++ Task2/Lab1_1/Shape/Dodecahedron.cpp
@@ -28,18 +28,64 @@ CDodecahedron::CDodecahedron()
 	points[0] = { 1.094f, 0.f, 0.676f };
 	points[1] = { 0.934f, -0.934f, 0.934f };//1
 	points[2] = { 1.512f, -0.577f, 0.0f };//2
-	points[3] = { 1.512f, 0.577f, 0.0f };
-	points[4] = { 0.934f, 0.934f, 0.934f };
-	points[5] = { 0.577f, 0.f, 1.512f };
+	points[3] = { 1.512f, 0.577f, 0.0f };//3
+	points[4] = { 0.934f, 0.934f, 0.934f };//4
+	points[5] = { 0.577f, 0.f, 1.512f };//5
 
 	CreatePentagon(points);
 
 	points[0] = { -1.094f, 0.f, 0.676f };
-	points[1] = { -0.934f, -0.934f, 0.934f };//1
-	points[2] = { -0.934f, 0.934f, 0.934f };
-	points[2] = { -1.512f, 0.577f, 0.0f };//
-	points[4] = { -1.512f, -0.577f, 0.0f };
-	points[5] = { -0.934f, -0.934f, 0.934f };
+	points[1] = { -1.512f, -0.577f, 0.0f };//2
+	points[2] = { -0.934f, -0.934f, 0.934f };//1
+	points[3] = { -0.577f, 0.f, 1.512f };//5
+	points[4] = { -0.934f, 0.934f, 0.934f };//4
+	points[5] = { -1.512f, 0.577f, 0.0f };//3
+
+	CreatePentagon(points);
+	///////////////////////////////////////
+	//              Y     Z   x
+	points[0] = { 0.f, 0.676f, -1.094f };
+	points[1] = { -0.934f, 0.934f, -0.934f };
+	points[2] = { 0.0f, 1.512f, -0.577f };
+	points[3] = { 0.934f, 0.934f, -0.934f };
+	points[4] = { 0.577f, 0.0f, -1.512f };
+	points[5] = { -0.577f, 0.0f, -1.512f };
+
+	CreatePentagon(points);
+
+	points[0] = { 0.f, -0.676f, -1.094f };
+	points[1] = { -0.577f, 0.0f, -1.512f };
+	points[2] = { 0.577f, 0.0f, -1.512f };
+	points[3] = { 0.934f, -0.934f, -0.934f };
+	points[4] = { 0.0f, -1.512f, -0.577f };
+	points[5] = { -0.934f, -0.934f, -0.934f };
+
+	CreatePentagon(points);
+	//              Y     Z   x
+	points[0] = { 1.094f, 0.f, -0.676f };
+	points[1] = { 0.577f, 0.f, -1.512f };//5
+	points[2] = { 0.934f, 0.934f,- 0.934f };//4
+	points[3] = { 1.512f, 0.577f, -0.0f };//3
+	points[4] = { 1.512f, -0.577f, -0.0f };//2
+	points[5] = { 0.934f, -0.934f, -0.934f };//1
+
+	CreatePentagon(points);
+
+	points[0] = { -1.094f, 0.f, -0.676f };
+	points[1] = { -1.512f, 0.577f, -0.0f };//3
+	points[2] = { -0.934f, 0.934f, -0.934f };//4
+	points[3] = { -0.577f, 0.f, -1.512f };//5
+	points[4] = { -0.934f, -0.934f, -0.934f };//1
+	points[5] = { -1.512f, -0.577f, -0.0f };//2
+
+	CreatePentagon(points);
+	/////////////////////////
+	points[0] = { -1.094f, 0.f, -0.676f };
+	points[1] = { -1.512f, 0.577f, -0.0f };//3
+	points[2] = { -0.934f, 0.934f, -0.934f };//4
+	points[3] = { -0.577f, 0.f, -1.512f };//5
+	points[4] = { -0.934f, -0.934f, -0.934f };//1
+	points[5] = { -1.512f, -0.577f, -0.0f };//2
 
 	CreatePentagon(points);
 }
