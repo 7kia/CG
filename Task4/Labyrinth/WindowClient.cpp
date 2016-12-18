83c92829d97da68d2b585a98821cdfda92f89480
7kia <il7kia@yandex.ru>
2016-12-18 13:03:02 +0300

    Fix bug when all was white

--- Task4/Labyrinth/WindowClient.cpp
+++ Task4/Labyrinth/WindowClient.cpp
@@ -20,9 +20,18 @@ void main(void)
 const char FRAGMENT_SHADER_PHONG[] = R"***(
 varying vec3 n;
 varying vec3 v;
+varying vec3 viewSpace;
+uniform sampler2D colormap;
+const vec4 fogColor = vec4(1, 1, 1, 1.0); 
+
 
 void main(void)
 {
+float dist = length(viewSpace); 
+
+float bi = (0.5- viewSpace.y) * 0.001;
+float insc = exp(-dist * bi); 
+
     vec4 result = vec4(0.0);
     for (int li = 0; li < gl_MaxLights; ++li)
     {
@@ -35,7 +44,8 @@ void main(void)
 
         float diffuseAngle = max(dot(n, lightDirection), 0.0);
         vec4 Idiff = gl_FrontLightProduct[li].diffuse * diffuseAngle;
-        Idiff = clamp(Idiff, 0.0, 1.0);
+        Idiff = clamp(Idiff, 0.0, 1.0) * 0.5;
+
 
         float specularAngle = max(dot(reflectDirection, viewDirection), 0.0);
         vec4 Ispec = gl_FrontLightProduct[li].specular
@@ -45,7 +55,8 @@ void main(void)
         result += Iamb + Idiff + Ispec;
     }
 
-    gl_FragColor = gl_FrontLightModelProduct.sceneColor + result;
+    gl_FragColor = gl_FrontLightModelProduct.sceneColor + texture2D(colormap, gl_TexCoord[0].st) * result - (fogColor * (1 - insc)) ;
+
 }
 )***";
 
